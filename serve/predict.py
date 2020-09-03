79f341846400705ed75845905e04cd0d6db3efce
radroid <raj9dholakia@gmail.com>
2020-09-03 12:02:02 +0000

    Final changes
    - Updated answers to the questions.
    - Other minor changes.

--- serve/predict.py
+++ serve/predict.py
@@ -69,9 +69,6 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
     
     data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
